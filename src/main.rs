2d29faaa98c7646b9117f3e1492e1d7b876d1b58
c ll <chris.llorca@gmail.com>
2023-02-06 11:31:26 -0500

    Learning axum - initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router
+};
+
+use std::net::SocketAddr;
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/", get(root));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing::info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
